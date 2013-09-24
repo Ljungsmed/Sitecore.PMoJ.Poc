1f671001491f5298c6e35a83fc6197d40dcd1c99
MoboDK <morten@bork.dk>
2013-09-24 11:56:38 +0200

    Login greeting

---
+++ Sitecore.PMoJ.Poc/HalloUser.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="HalloUser.ascx.cs" Inherits="Sitecore.PMoJ.Poc.HalloUser" %>
+<%@ Register TagPrefix="sc" Namespace="Sitecore.Web.UI.WebControls" Assembly="Sitecore.Kernel" %>
+<asp:Literal runat="server" ID="ltUser"></asp:Literal> <asp:Button runat="server" ID="btLogout" Text="Logout" OnClick="btLogout_OnClick"/>
---
+++ Sitecore.PMoJ.Poc/HalloUser.ascx.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Links;
+
+namespace Sitecore.PMoJ.Poc
+{
+    using System;
+
+    public partial class HalloUser : System.Web.UI.UserControl
+    {
+        private void Page_Load(object sender, EventArgs e)
+        {
+            if (Sitecore.Context.IsLoggedIn)
+            {
+                ltUser.Text = "Welcome " + Sitecore.Context.User.Name;
+                btLogout.Visible = true;
+            }
+            else
+            {
+                ltUser.Text = "Please login";
+                btLogout.Visible = false;
+            }
+            // Put user code to initialize the page here
+        }
+
+        protected void btLogout_OnClick(object sender, EventArgs e)
+        {
+            Sitecore.Security.Authentication.AuthenticationManager.Logout();
+            Response.Redirect(LinkManager.GetItemUrl(Sitecore.Context.Item));
+        }
+    }
+}
